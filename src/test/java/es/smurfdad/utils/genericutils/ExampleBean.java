29ece58474c7e49c8240d4abce95f368df493cad
@Smurf_Dad <@Smurf_Dad>
2017-11-14 18:04:58 +0100

    Ajustamos parent

--- src/test/java/es/smurfdad/utils/genericutils/ExampleBean.java
+++ src/test/java/es/smurfdad/utils/genericutils/ExampleBean.java
@@ -2,4 +2,14 @@ package es.smurfdad.utils.genericutils;
 
 public class ExampleBean {
 
+    private String id;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
 }
